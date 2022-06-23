dabec0165da217399592b9726a7196d5459b09a2
lilv98 <zhenwei.tang@kaust.edu.sa>
2022-06-23 19:50:55 +0300

    fix_data_leakage

--- code/pulearning.py
+++ code/pulearning.py
@@ -80,13 +80,13 @@ class DrugTreatmentPU(torch.nn.Module):
         self.prior = cfg.prior
         self.loss_type = cfg.loss_type
         self.base_model = cfg.base_model
-        self.fc_1 = torch.nn.Linear(cfg.emb_dim, cfg.emb_dim)
-        self.fc_2 = torch.nn.Linear(cfg.emb_dim, 1)
+        self.fc_1 = torch.nn.Linear(cfg.emb_dim, 1)
+        # self.fc_2 = torch.nn.Linear(cfg.emb_dim//2, 1)
         self.e_embedding = torch.nn.Embedding(len(e_dict), cfg.emb_dim)
         self.r_embedding = torch.nn.Embedding(len(r_dict), cfg.emb_dim)
 
         torch.nn.init.xavier_uniform_(self.fc_1.weight.data)
-        torch.nn.init.xavier_uniform_(self.fc_2.weight.data)
+        # torch.nn.init.xavier_uniform_(self.fc_2.weight.data)
         torch.nn.init.xavier_uniform_(self.e_embedding.weight.data)
         torch.nn.init.xavier_uniform_(self.r_embedding.weight.data)
 
@@ -106,6 +106,9 @@ class DrugTreatmentPU(torch.nn.Module):
 
     def _DistMult(self, h_emb, r_emb, t_emb):
         return (h_emb * r_emb * t_emb).sum(dim=-1)
+    
+    def _TranE(self, h_emb, r_emb, t_emb):
+        pass
     
     def _forward_kg(self, data):
         h_emb = self.e_embedding(data[:, :, 0])
@@ -130,22 +133,36 @@ class DrugTreatmentPU(torch.nn.Module):
         neg = torch.index_select(data, 0, (data[:, 1] == 0).nonzero().squeeze(-1))
         e_emb_pos = self.e_embedding(pos[:, 0])
         e_emb_neg = self.e_embedding(neg[:, 0])
-        pred_pos = self.fc_2(torch.nn.functional.leaky_relu(self.do(self.fc_1(e_emb_pos))))
-        pred_neg = self.fc_2(torch.nn.functional.leaky_relu(self.do(self.fc_1(e_emb_neg))))
+        # pred_pos = self.fc_2(torch.nn.functional.leaky_relu(self.do(self.fc_1(e_emb_pos))))
+        # pred_neg = self.fc_2(torch.nn.functional.leaky_relu(self.do(self.fc_1(e_emb_neg))))
+        pred_pos = self.fc_1(e_emb_pos)
+        pred_neg = self.fc_1(e_emb_neg)
         return pred_pos, pred_neg
     
     def get_loss_tr(self, data):
         pred_pos, pred_neg = self._forward_tr(data)
         loss_pos = - torch.nn.functional.logsigmoid(pred_pos).mean()
         loss_neg = - torch.log(1 - torch.sigmoid(pred_neg) + 1e-10).mean()
-        return (loss_pos + loss_neg) / 2
+        return (loss_pos * len(pred_pos) + loss_neg * len(pred_neg)) / (len(pred_pos) + len(pred_neg))
     
     def predict(self, data):
         e_emb = self.e_embedding(data[:, 0])
-        return torch.sigmoid(self.fc_2(torch.nn.functional.leaky_relu(self.do(self.fc_1(e_emb)))))
-
+        return torch.sigmoid(self.fc_1(e_emb))
+        # return torch.sigmoid(self.fc_2(torch.nn.functional.leaky_relu(self.do(self.fc_1(e_emb)))))
 
 def read_data(cfg):
+    
+    test_data = []
+    with open(cfg.root + cfg.dataset + '/test_' + cfg.fold + '.ttl') as f:
+        for line in f:
+            line = line.strip().split('\t')
+            if line[2] == 'ex:effective':
+                test_data.append([line[0], 1])
+            elif line[2] == 'ex:low_effect':
+                test_data.append([line[0], 0])
+            else:
+                raise ValueError
+    
     train_data_kg = []
     train_data_tr = []
     with open(cfg.root + cfg.dataset + '/train_' + cfg.fold + '.ttl') as f:
@@ -158,23 +175,24 @@ def read_data(cfg):
                     train_data_tr.append([line[0], 0])
                 else:
                     train_data_kg.append(line)
-                    # raise ValueError
+            elif line[1] == 'ex:no_belong_to':
+                if line[2] == 'ex:low_effect':
+                    train_data_tr.append([line[0], 1])
+                elif line[2] == 'ex:effective':
+                    train_data_tr.append([line[0], 0])
+                else:
+                    train_data_kg.append(line)
             else:
                 train_data_kg.append(line)
+    
+    for train in train_data_tr:
+        for test in test_data:
+            if train == test:
+                train_data_tr.remove(train)    
+    
+    test_data = pd.DataFrame(test_data, columns=['tr', 'label'])
     train_data_kg = pd.DataFrame(train_data_kg, columns=['h', 'r', 't'])
     train_data_tr = pd.DataFrame(train_data_tr, columns=['tr', 'label'])
-    
-    test_data = []
-    with open(cfg.root + cfg.dataset + '/test_' + cfg.fold + '.ttl') as f:
-        for line in f:
-            line = line.strip().split('\t')
-            if line[2] == 'ex:effective':
-                test_data.append([line[0], 1])
-            elif line[2] == 'ex:low_effect':
-                test_data.append([line[0], 0])
-            else:
-                raise ValueError
-    test_data = pd.DataFrame(test_data, columns=['tr', 'label'])
     
     all_e = set(train_data_kg['h'].unique()) | set(train_data_kg['t'].unique()) | set(train_data_tr['tr'].unique()) | set(test_data['tr'].unique())
     all_r = set(train_data_kg['r'].unique())
@@ -199,31 +217,31 @@ def read_data(cfg):
     
     return e_dict, r_dict, train_data_kg, train_data_tr, test_data, already_ts_dict, already_hs_dict
 
-def get_f1(labels, preds):
-    N = len(labels)
-    as_pos = (preds > 0.5).astype(int)
-    as_neg = (preds < 0.5).astype(int)
-    tp = (as_pos * labels).sum() 
-    fp = (as_pos * (1 - labels)).sum()
-    fn = (as_neg * labels).sum()
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    f1 = (2 * precision * recall) / (precision + recall)
-    return round(precision, 4), round(recall, 4), round(f1, 4)
+# def get_f1(labels, preds):
+#     N = len(labels)
+#     as_pos = (preds > 0.5).astype(int)
+#     as_neg = (preds < 0.5).astype(int)
+#     tp = (as_pos * labels).sum() 
+#     fp = (as_pos * (1 - labels)).sum()
+#     fn = (as_neg * labels).sum()
+#     precision = tp / (tp + fp)
+#     recall = tp / (tp + fn)
+#     f1 = (2 * precision * recall) / (precision + recall)
+#     return round(precision, 4), round(recall, 4), round(f1, 4)
 
 def parse_args(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--root', default='../data/', type=str)
-    parser.add_argument('--dataset', default='G3', type=str)
-    parser.add_argument('--fold', default='2', type=str)
+    parser.add_argument('--dataset', default='G2', type=str)
+    parser.add_argument('--fold', default='1', type=str)
     # Tunable
     parser.add_argument('--bs', default=256, type=int)
     parser.add_argument('--lr', default=0.001, type=float)
-    parser.add_argument('--wd', default=0, type=float)
-    parser.add_argument('--do', default=0.2, type=float)
-    parser.add_argument('--prior', default=0.01, type=float)
-    parser.add_argument('--emb_dim', default=512, type=int)
-    parser.add_argument('--loss_type', default='pn', type=str)
+    parser.add_argument('--wd', default=0.0001, type=float)
+    parser.add_argument('--do', default=0.5, type=float)
+    parser.add_argument('--prior', default=0.001, type=float)
+    parser.add_argument('--emb_dim', default=64, type=int)
+    parser.add_argument('--loss_type', default='pu', type=str)
     parser.add_argument('--num_ng', default=4, type=int)
     parser.add_argument('--lmbda', default=1, type=float)
     parser.add_argument('--base_model', default='DistMult', type=str)
@@ -234,7 +252,7 @@ def parse_args(args=None):
     parser.add_argument('--gpu', default=0, type=int)
     parser.add_argument('--valid_interval', default=5, type=int)
     parser.add_argument('--verbose', default=0, type=int)
-    parser.add_argument('--tolerance', default=3, type=int)
+    parser.add_argument('--tolerance', default=20, type=int)
     return parser.parse_args(args)
 
 if __name__ == '__main__':
@@ -260,7 +278,7 @@ if __name__ == '__main__':
                                                     shuffle=True, 
                                                     drop_last=True)
     train_dataloader_tr = torch.utils.data.DataLoader(dataset=train_dataset_tr, 
-                                                    batch_size=cfg.bs//4, 
+                                                    batch_size=cfg.bs, 
                                                     num_workers=cfg.num_workers, 
                                                     shuffle=True, 
                                                     drop_last=True)
@@ -272,14 +290,14 @@ if __name__ == '__main__':
     model = DrugTreatmentPU(e_dict, r_dict, cfg)
     model = model.to(device)
     tolerance = cfg.tolerance
-    max_auc = 0
+    max_value = 0
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr, weight_decay=cfg.wd)
     if cfg.verbose == 1:
         train_dataloader_kg = tqdm.tqdm(train_dataloader_kg)
         train_dataloader_tr = tqdm.tqdm(train_dataloader_tr)
         test_dataloader = tqdm.tqdm(test_dataloader)
     for epoch in range(cfg.max_epochs):
-        print(f'Epoch {epoch + 1}:')
+        # print(f'Epoch {epoch + 1}:')
         model.train()
         avg_loss = []
         for batch in zip(train_dataloader_kg, train_dataloader_tr):
@@ -293,27 +311,31 @@ if __name__ == '__main__':
             optimizer.step()
             avg_loss.append(loss.item())
         avg_loss = round(sum(avg_loss)/len(avg_loss), 6)
-        print(f'Loss: {avg_loss}')
+        # print(f'Loss: {avg_loss}')
         if (epoch + 1) % cfg.valid_interval == 0:
             labels = []
             preds = []
             with torch.no_grad():
                 model.eval()
-                # precision, recall, f1 (T = 0.5)
                 for batch in test_dataloader:
                     labels.append(batch[:, 1])
                     batch = batch.to(device)
                     preds.append(model.predict(batch))
             labels, preds = torch.cat(labels).numpy(), torch.cat(preds).cpu().numpy().flatten()
             auc = round(roc_auc_score(labels, preds), 4)
-            precision, recall, f1 = get_f1(labels, preds)
-            print(f'AUC:{auc}\tPrecision:{precision}\tRecall:{recall}\tF1:{f1}')
-            if auc > max_auc:
-                max_auc = auc
+            aupr = round(average_precision_score(labels, preds), 4)
+            precision, recall, thresholds = precision_recall_curve(labels, preds)
+            f1_scores = 2 * recall * precision / (recall + precision)
+            fmax = round(np.max(f1_scores), 4)
+            th = thresholds[np.argmax(f1_scores)]
+            print(f'AUC:{auc}\tAUPR:{aupr}\tFmax:{fmax}\tTh:{th}')
+            # precision, recall, f1 = get_f1(labels, preds)
+            # print(f'AUC:{auc}\tAUPR:{aupr}\tPrecision:{precision}\tRecall:{recall}\tF1:{f1}')
+            if (auc + aupr + fmax) > max_value:
+                max_value = (auc + aupr + fmax)
                 tolerance = cfg.tolerance
             else:
                 tolerance -= 1
-            # if auc == 1:
-            #     print(labels)
-            #     print(preds)
+            if tolerance == 0:
+                break
 
